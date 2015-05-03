b2f528d4c7e123b7de11eeb7ecf03523eb7e5f7d
Vladimir Vovk <vladimir@vovk.in>
2015-05-03 17:39:51 +0700

    Add js-commint - node.js eval and repl

--- lisp/init-java-script.el
+++ lisp/init-java-script.el
@@ -18,5 +18,25 @@
       (require 'tern-auto-complete)
       (tern-ac-setup)))
 
+;; js-comint node.js repl and code execution -> M-x run-js
+(require 'js-comint)
+    (setq inferior-js-program-command "node") ;; not "node-repl"
+    ;; Use your favorited js mode here:
+    (add-hook 'js2-mode-hook '(lambda ()
+                (local-set-key "\C-x\C-e"
+                                'js-send-last-sexp)
+                (local-set-key "\C-\M-x"
+                                'js-send-last-sexp-and-go)
+                (local-set-key "\C-cb"
+                                'js-send-buffer)
+                (local-set-key "\C-c\C-b"
+                                'js-send-buffer-and-go)
+                (local-set-key "\C-cl"
+                                'js-load-file-and-go)
+                ))
+
+;; fix bug with node.js command prompt
+(setenv "NODE_NO_READLINE" "1")
+
 (provide 'init-java-script)
 ;;; init-java-script ends here
